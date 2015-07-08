74759a7ee0f920594e75647278f4ff960311f246
Evan Kroske <evankroske@google.com>
2015-07-07 18:52:53 -0700

    Add code to add disclosure button to callout.

--- Hazardous Waste Disposal Sites in Mountain View/LocationMapViewController.mm
+++ Hazardous Waste Disposal Sites in Mountain View/LocationMapViewController.mm
@@ -17,11 +17,33 @@
     mapView.region = MKCoordinateRegionMakeWithDistance({37.3894, -122.0819}, 10000, 10000);
     MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
     annotation.coordinate = {37.3894, -122.0819};
+    annotation.title = @"Mountain View";
     [mapView addAnnotation:annotation];
 }
-
-- (void)mapView:(MKMapView *)mapView didSelectAnnotationView:(MKAnnotationView *)view {
-    NSLog(@"%s", "called");
+- (MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id <MKAnnotation>)annotation
+{
+    MKPinAnnotationView *pinView = (MKPinAnnotationView*)[mapView
+                                                             dequeueReusableAnnotationViewWithIdentifier:@"CustomPinAnnotationView"];
+    
+    if (!pinView)
+    {
+        pinView = [[MKPinAnnotationView alloc]
+                                              initWithAnnotation:annotation reuseIdentifier:@"CustomPinAnnotationView"];
+        pinView.pinColor = MKPinAnnotationColorPurple;
+        pinView.animatesDrop = YES;
+        pinView.canShowCallout = YES;
+        
+        // Because this is an iOS app, add the detail disclosure button to display details about the annotation in another view.
+        UIButton *rightButton = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+        [rightButton addTarget:nil action:nil forControlEvents:UIControlEventTouchUpInside];
+        pinView.rightCalloutAccessoryView = rightButton;        
+    }
+    else {
+        pinView.annotation = annotation;
+    }
+    
+    return pinView;
 }
+
 
 @end
